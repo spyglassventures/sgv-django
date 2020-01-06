df5b70525f60e68e48caaaf3bb7666fdff7b177c
Dr. Daniel Müller <dm@spyglassventures.ch>
2020-01-06 10:41:44 +0100

    Update settings.py

--- learning_users/settings.py
+++ learning_users/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
